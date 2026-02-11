667e43a69206a66d12884bb366c96b024a7453cc
Tony-DAgosto <tdagosto2@huskers.unl.edu>
2026-02-11 15:55:57 -0600

    Added comments

--- image_pipeline.py
+++ image_pipeline.py
@@ -6,14 +6,14 @@ load_dotenv()
 client = OpenAI()
 
 def generate_response(pdf_path):
-    file = client.files.create(
-        file=open(pdf_path, "rb"),
-        purpose="user_data"
+    file = client.files.create( #Tells OpenAI API to create a file object which holds the pdf file content
+        file=open(pdf_path, "rb"), #Opens said file in read-binary (rb) mode
+        purpose="user_data" #Tells API that the file is uploaded for the purpose of fufilling a user request
     )
 
     messages = [
-        {"role": "system", "content": "You are a researcher who specializes in analyzing research papers."},
-        {"role": "user", "content": [ {"type": "file", "file": {"file_id": file.id}},
+        {"role": "system", "content": "You are a researcher who specializes in analyzing research papers."}, #Gives API base instructions to systm type, which is the AI's role in this convo
+        {"role": "user", "content": [ {"type": "file", "file": {"file_id": file.id}}, #Tells API the input is a file type, and gives it the file id of the uploaded pdf
                                      {"type": "text", "text": "Give me a concise, informative summary of the attached document. 500 words or less."}
     ]}
     ]
@@ -26,7 +26,7 @@ def generate_response(pdf_path):
     )
 
     result = response.choices[0].message.content
-    client.files.delete(file.id)
+    client.files.delete(file.id) #Deletes the file from the API to save storage and tokens
     return result
 
 if __name__ == "__main__":
